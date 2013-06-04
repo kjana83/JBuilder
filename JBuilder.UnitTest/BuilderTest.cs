c18800f0eaedf5f09fb9afa2a50f303e316c1c81
Janarthanan K <janarthanan.kanagaraj@gmail.com>
2013-06-04 22:52:45 +0530

    Initial Commit

---
+++ JBuilder.UnitTest/BuilderTest.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+
+namespace JBuilder.UnitTest
+{
+    [TestFixture]
+    public class BuilderTest
+    {
+
+        [TestCase]
+        public void Should_Create_Simple_Object()
+        {
+            Address address = Builder.BuildSingle(typeof(Address));
+            Assert.IsNotNullOrEmpty(address.AddressLine1);
+            Assert.IsNotNullOrEmpty(address.AddressLine2);
+        }
+
+        [TestCase]
+        public void Should_Create_Nested_Object()
+        {
+            Person person = Builder.BuildSingle(typeof(Person));
+            Assert.IsNotNullOrEmpty(person.FirstName);
+            Assert.IsNotNull(person.Address);
+            Assert.IsNotNullOrEmpty(person.Address.AddressLine1);
+        }
+    }
+
+    public class Person
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public Address Address { get; set; }
+    }
+
+    public class Address
+    {
+        public string AddressLine1 { get; set; }
+
+        public string AddressLine2 { get; set; }
+    }
+}
---
+++ JBuilder/Builder.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JBuilder
+{
+    public static class Builder
+    {
+        public static dynamic BuildSingle(Type type)
+        {
+            dynamic TObject = Activator.CreateInstance(type); // default(T);
+            IList<PropertyInfo> props = TObject.GetType().GetProperties();
+            props.ToList().ForEach(prop =>
+                {
+                    if (prop.PropertyType.Namespace == "System")
+                    {
+                        TObject.GetType().InvokeMember(prop.Name, BindingFlags.SetProperty, null,
+                            TObject, new object[] { prop.Name + "1" });
+                    }
+                    else
+                    {
+                        
+                        TObject.GetType().InvokeMember(prop.Name, BindingFlags.SetProperty, null,
+                            TObject, new object[] {BuildSingle(prop.PropertyType.UnderlyingSystemType)});                        
+                    }
+
+                });
+            return TObject;
+        }
+    }
+}
